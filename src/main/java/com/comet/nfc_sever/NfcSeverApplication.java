2c29cb48e11146e5194fd12b7a6fa0e9ebd3890e
choi-hyeseong <bd2845@kakao.com>
2023-01-18 11:47:49 +0900

    Initial commit

---
+++ src/main/java/com/comet/nfc_sever/NfcSeverApplication.java
@@ -0,0 +1,13 @@
+package com.comet.nfc_sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfcSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfcSeverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comet/nfc_sever/NfcSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comet.nfc_sever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfcSeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
