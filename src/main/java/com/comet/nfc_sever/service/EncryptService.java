6a6cbca66ddd169f99588529148065ab2c271486
choi-hyeseong <bd2845@kakao.com>
2024-12-31 18:47:00 +0900

    feat: support cbc aes encryption

--- src/main/java/com/comet/nfc_sever/service/EncryptService.java
+++ src/main/java/com/comet/nfc_sever/service/EncryptService.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.Charset;
 import java.security.*;
+import java.util.Arrays;
 import java.util.Base64;
 
 @Service
@@ -14,6 +16,7 @@ import java.util.Base64;
 public class EncryptService {
 
     private KeyPair keyPair;
+    private static int IV_LEN = 16;
 
     private void initKeyPair() {
         try {
@@ -64,12 +67,18 @@ public class EncryptService {
 
     //AES 는 16, 24, 32 바이트여야 암호화 가ㅡㄴㅇ
     public String AESEncrypt(String input, String key) {
+        byte[] iv = new byte[16];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
         Key secretKeySpec = new SecretKeySpec(key.substring(0, 16).getBytes(), "AES");
         try {
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(iv));
             byte[] encrypt = cipher.doFinal(input.getBytes());
-            return Base64.getUrlEncoder().encodeToString(encrypt);
+            byte[] result = new byte[iv.length + encrypt.length];
+            System.arraycopy(cipher.getIV(), 0, result, 0, iv.length);
+            System.arraycopy(encrypt, 0, result, iv.length, encrypt.length);
+            return Base64.getUrlEncoder().encodeToString(result);
         }
         catch (Exception e) {
             log.error(e.getLocalizedMessage());
@@ -80,10 +89,13 @@ public class EncryptService {
     public String AESDecrypt(String input, String key) {
         Key secretKeySpec = new SecretKeySpec(key.substring(0, 16).getBytes(), "AES");
         try {
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
             byte[] urlDecode = Base64.getUrlDecoder().decode(input.getBytes());
-            byte[] decrypt = cipher.doFinal(urlDecode);
+
+            byte[] iv = Arrays.copyOfRange(urlDecode, 0, IV_LEN);
+            byte[] data = Arrays.copyOfRange(urlDecode, IV_LEN, urlDecode.length);
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(iv));
+            byte[] decrypt = cipher.doFinal(data);
             return new String(decrypt);
         }
         catch (Exception e) {
